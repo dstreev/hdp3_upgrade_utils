b55efbfb969976c366ea79eb0392ccf60944fcce
David W. Streever <david@streever.com>
2019-06-19 17:26:23 -0400

    locating transactional table directories.

---
+++ acid_table_compaction_check.sql
@@ -0,0 +1,31 @@
+-- ACID Table Details
+--
+-- Variables:
+-- DB - The database you placed the hms dump table.
+-- ENV - IE: dev,qa,prod.  Used to support multiple
+-- environment dump files in the same database.
+USE ${DB};
+
+-- List ACID Tables.
+WITH directories AS (
+                    SELECT DISTINCT
+                        DB_NAME
+                      , TBL_NAME
+                      , PART_NAME
+                      , CASE
+                            WHEN PART_NAME IS NULL
+                                THEN regexp_extract(tbl_location, 'hdfs://([^/]+)(.*)', 2)
+                            WHEN PART_NAME IS NOT NULL
+                                THEN regexp_extract(part_location, 'hdfs://([^/]+)(.*)', 2)
+                        END AS LOCATION
+                    FROM
+                        HMS_DUMP_${ENV}
+                    WHERE
+                          TBL_PARAM_KEY = 'transactional'
+                      AND TBL_PARAM_VALUE = 'true'
+                    )
+SELECT *, SIZE(SPLIT(LOCATION,"/"))
+FROM
+    directories
+ORDER BY LOCATION, length(LOCATION);
+
