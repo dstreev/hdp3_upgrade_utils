69628781555cc62ce3fa0c038736ceaf8dcc0b3a
David W. Streever <david@streever.com>
2019-06-28 12:11:52 +0800

    Additional clarification to the post-migration steps.

--- post_migration.sh
+++ post_migration.sh
@@ -15,10 +15,11 @@ while read line; do
     POST_DB=`echo ${line} | cut -f 1 -d " "`
     echo "Launching Post Migration for: ${POST_DB}"
 	RUN_CMD="${HIVE_CMD} \
-	--config /etc/hive/conf --service strictmanagedmigration \
-    --hiveconf hive.strict.managed.tables=true  \
-    -m automatic --dbRegex ${POST_DB} --tableRegex .* ${DO_DRYRUN} \
-    --modifyManagedTables --oldWarehouseRoot /apps/hive/warehouse"
+--config /etc/hive/conf --service strictmanagedmigration \
+--hiveconf hive.strict.managed.tables=true  \
+-m automatic --dbRegex ${POST_DB} --tableRegex .* ${DO_DRYRUN} \
+--modifyManagedTables --oldWarehouseRoot /apps/hive/warehouse"
 	echo "Running: ${RUN_CMD}"
 	nohup ${RUN_CMD} &
+	echo "tail /tmp/${USER}/hive.log to see progress..."
 done < ${OUTPUT_DIR}/post_migration.txt
